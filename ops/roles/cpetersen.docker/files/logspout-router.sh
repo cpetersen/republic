708290d05d29d223b350af9e3a18339c478b9bbc
Christopher Petersen <chris@petersen.io>
2015-02-21 07:13:42 -0800

    Removing keys from the ansible plays

--- ops/roles/cpetersen.docker/files/logspout-router.sh
+++ ops/roles/cpetersen.docker/files/logspout-router.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl localhost:8000/routes -X POST -d '{"target": {"type": "rfc5424", "addr": "api.logentries.com:10000", "structured_data": "e111fb1e-3d4d-44b5-a61a-8fa6c610cdf8"}}'
+curl localhost:8000/routes -X POST -d '{"target": {"type": "rfc5424", "addr": "api.logentries.com:10000", "structured_data": "$1"}}'
